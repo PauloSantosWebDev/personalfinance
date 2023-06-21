29484dfa0dd1a69a1cbcde7536426d4a30db8e53
Paulo <paulosantos@outlook.com.au>
2023-06-21 21:21:58 +0930

    Database and first table created. Post method to feed the table also created.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const db = require('./db');
 const http = require('http');
 const path = require('path');
 const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -21,11 +23,50 @@ app.set('view engine', 'njk');
 app.use(express.static(path.join(__dirname,'node_modules')));
 app.use(express.static(path.join(__dirname,'public')));
 
+//Middleware to parse request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //Creating routes
+//Get methods
 app.get("/", (req, res) =>{
     res.render('index', {title: 'Home page'});
 })
 
+app.get("/categincome", (req, res) =>{
+    res.render('categincome.njk', {title: 'New income category'});
+})
+
+//Post methods
+app.post('/categincome', (req, res) =>{
+    const incomeCategory = req.body.inputIncomeCategory;
+    const description = req.body.inputIncomeCategoryDescription;
+
+    console.log('My income category is: ' + incomeCategory);
+
+    //Command to insert new customes to the customers table in the hydroil.sqlite database
+    db.run('INSERT INTO income_categories (category, description) VALUES (?, ?)', [incomeCategory, description], (err) => {
+        if (err) {
+        console.error(err.message);
+        res.status(500).send('Error updating data in customer table.');
+        } else {
+        res.status(200);
+        console.log('Data updated successfully in customers table.');
+        res.redirect('/categincome');
+        }
+    });
+
+    db.all('SELECT * FROM income_categories', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach(row => {
+            console.log(row.category_id, row.category, row.description);
+        })
+    })
+})
+
+//Server listening
 const server = http.createServer(app);
 server.listen('3000', (err, html) => {
     console.log('Listening on port 3000');
---
+++ db.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+
+//open the database connection
+let db = new sqlite3.Database("personalfinances.sqlite", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the database.");
+})
+
+//Creating the tables needed if needed.
+//Income categories table
+db.run('CREATE TABLE IF NOT EXISTS income_categories (category_id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT NOT NULL, description TEXT)');
+
+//Example of how to create a table with a foreign key
+// db.run('CREATE TABLE IF NOT EXISTS contacts (customer_id INTEGER NOT NULL, contact_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, phone_number TEXT, mobile_number TEXT, FOREIGN KEY (customer_id) REFERENCES customers(customer_id))');
+
+module.exports = db;
